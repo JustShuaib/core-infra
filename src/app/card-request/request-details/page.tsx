7d2706d3735d975995b99a90e003a31e07304612
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2025-02-22 01:25:01 +0100

    added suspense

--- src/app/card-request/request-details/page.tsx
+++ src/app/card-request/request-details/page.tsx
@@ -10,7 +10,7 @@ import {useDataContext} from "@/services/context";
 import {getStatusColor} from "@/utils/getStatusColor";
 import {Button} from "@mui/material";
 import {useSearchParams} from "next/navigation";
-import {useState} from "react";
+import {Suspense, useState} from "react";
 import {TextFieldElement, useForm} from "react-hook-form-mui";
 interface RequestDetailsProps {
   branchName: string;
@@ -38,184 +38,186 @@ const RequestDetails = () => {
   });
 
   return (
-    <div className="p-6">
-      <p className="text-lg font-medium">Request Details</p>
-      <p className="text-gray-600">
-        Perform predetermined actions on card requests here.
-      </p>
-      <form noValidate>
-        <div className="bg-white border my-6 p-4 rounded-[10px]">
-          <p className="text-lg font-medium mb-6">Card Request Details</p>
-          <div className="grid grid-cols-2 gap-6 mb-12 gap-x-12">
-            <TextFieldElement
-              name={"branchName"}
-              variant="outlined"
-              label="Branch Name"
-              placeholder={"Enter branch name"}
-              control={control}
-              disabled
-              required
-              fullWidth
-              sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
-            />
-            <TextFieldElement
-              name={"initiator"}
-              variant="outlined"
-              placeholder="00000000"
-              label={"Initiator"}
-              control={control}
-              disabled
-              required
-              fullWidth
-              sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
-            />
-            <TextFieldElement
-              name={"cardType"}
-              variant="outlined"
-              placeholder="00000000"
-              label={"Card Type"}
-              control={control}
-              disabled
-              required
-              fullWidth
-              sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
-            />
-            <TextFieldElement
-              name={"cardCharges"}
-              variant="outlined"
-              placeholder="00000000"
-              label={"Card Charges"}
-              control={control}
-              disabled
-              required
-              fullWidth
-              sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
-            />
-            <TextFieldElement
-              name={"quantity"}
-              variant="outlined"
-              placeholder="00000000"
-              label={"Quantity"}
-              control={control}
-              disabled
-              required
-              fullWidth
-              sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
-            />
-            <TextFieldElement
-              name={"batch"}
-              variant="outlined"
-              placeholder="00000000"
-              label={"Batch"}
-              control={control}
-              disabled
-              required
-              fullWidth
-              sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
-            />
-            <div>
-              <span className="text-gray-700">Date Requested</span>
-              <p className="text-gray-900 mt-2 font-medium">
-                {currentRequest?.dateRequested}
-              </p>
-            </div>
-            <div>
-              <p>Status</p>
-              <CustomChip
-                label={currentRequest?.status || ""}
-                color={getStatusColor(currentRequest?.status || "default")}
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="p-6">
+        <p className="text-lg font-medium">Request Details</p>
+        <p className="text-gray-600">
+          Perform predetermined actions on card requests here.
+        </p>
+        <form noValidate>
+          <div className="bg-white border my-6 p-4 rounded-[10px]">
+            <p className="text-lg font-medium mb-6">Card Request Details</p>
+            <div className="grid grid-cols-2 gap-6 mb-12 gap-x-12">
+              <TextFieldElement
+                name={"branchName"}
+                variant="outlined"
+                label="Branch Name"
+                placeholder={"Enter branch name"}
+                control={control}
+                disabled
+                required
+                fullWidth
+                sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
               />
+              <TextFieldElement
+                name={"initiator"}
+                variant="outlined"
+                placeholder="00000000"
+                label={"Initiator"}
+                control={control}
+                disabled
+                required
+                fullWidth
+                sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
+              />
+              <TextFieldElement
+                name={"cardType"}
+                variant="outlined"
+                placeholder="00000000"
+                label={"Card Type"}
+                control={control}
+                disabled
+                required
+                fullWidth
+                sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
+              />
+              <TextFieldElement
+                name={"cardCharges"}
+                variant="outlined"
+                placeholder="00000000"
+                label={"Card Charges"}
+                control={control}
+                disabled
+                required
+                fullWidth
+                sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
+              />
+              <TextFieldElement
+                name={"quantity"}
+                variant="outlined"
+                placeholder="00000000"
+                label={"Quantity"}
+                control={control}
+                disabled
+                required
+                fullWidth
+                sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
+              />
+              <TextFieldElement
+                name={"batch"}
+                variant="outlined"
+                placeholder="00000000"
+                label={"Batch"}
+                control={control}
+                disabled
+                required
+                fullWidth
+                sx={{bgcolor: "#F5F5F7", borderRadius: "8px"}}
+              />
+              <div>
+                <span className="text-gray-700">Date Requested</span>
+                <p className="text-gray-900 mt-2 font-medium">
+                  {currentRequest?.dateRequested}
+                </p>
+              </div>
+              <div>
+                <p>Status</p>
+                <CustomChip
+                  label={currentRequest?.status || ""}
+                  color={getStatusColor(currentRequest?.status || "default")}
+                />
+              </div>
+            </div>
+            <div>
+              <p className="font-medium">Actions</p>
+              <div className="flex justify-around items-center mt-2">
+                <Button
+                  startIcon={<FileDownload />}
+                  disabled={status !== "pending"}
+                  onClick={() => setOpenDownloadModal(true)}
+                  variant="contained"
+                  sx={{
+                    bgcolor: "#344054",
+                    textTransform: "none",
+                    minWidth: "max-content",
+                  }}
+                >
+                  Download for Production
+                </Button>
+                <Button
+                  startIcon={<LoadingIcon />}
+                  disabled={status !== "downloaded"}
+                  variant="contained"
+                  onClick={() => setStatus("in-progress")}
+                  sx={{
+                    bgcolor: "#B54708",
+                    textTransform: "none",
+                    maxWidth: "fit-content",
+                  }}
+                >
+                  Mark as In Progress
+                </Button>
+                <Button
+                  startIcon={<PackageCheckIcon />}
+                  variant="contained"
+                  disabled={status !== "in-progress"}
+                  onClick={() => setStatus("ready")}
+                  sx={{
+                    bgcolor: "#067647",
+                    textTransform: "none",
+                    minWidth: "max-content",
+                  }}
+                >
+                  Mark as Ready
+                </Button>
+                <Button
+                  startIcon={<PackageSent />}
+                  onClick={() => setOpenDispatchModal(true)}
+                  disabled={status !== "ready"}
+                  variant="contained"
+                  sx={{
+                    bgcolor: "#8020E7",
+                    textTransform: "none",
+                    minWidth: "max-content",
+                  }}
+                >
+                  Send to Dispatch
+                </Button>
+                <Button
+                  startIcon={<CheckCircleBroken />}
+                  onClick={() => setStatus("acknowledged")}
+                  disabled={status !== "sent"}
+                  variant="contained"
+                  sx={{
+                    bgcolor: "#014DAF",
+                    textTransform: "none",
+                    minWidth: "max-content",
+                  }}
+                >
+                  Mark as Acknowledged
+                </Button>
+              </div>
             </div>
           </div>
-          <div>
-            <p className="font-medium">Actions</p>
-            <div className="flex justify-around items-center mt-2">
-              <Button
-                startIcon={<FileDownload />}
-                disabled={status !== "pending"}
-                onClick={() => setOpenDownloadModal(true)}
-                variant="contained"
-                sx={{
-                  bgcolor: "#344054",
-                  textTransform: "none",
-                  minWidth: "max-content",
-                }}
-              >
-                Download for Production
-              </Button>
-              <Button
-                startIcon={<LoadingIcon />}
-                disabled={status !== "downloaded"}
-                variant="contained"
-                onClick={() => setStatus("in-progress")}
-                sx={{
-                  bgcolor: "#B54708",
-                  textTransform: "none",
-                  maxWidth: "fit-content",
-                }}
-              >
-                Mark as In Progress
-              </Button>
-              <Button
-                startIcon={<PackageCheckIcon />}
-                variant="contained"
-                disabled={status !== "in-progress"}
-                onClick={() => setStatus("ready")}
-                sx={{
-                  bgcolor: "#067647",
-                  textTransform: "none",
-                  minWidth: "max-content",
-                }}
-              >
-                Mark as Ready
-              </Button>
-              <Button
-                startIcon={<PackageSent />}
-                onClick={() => setOpenDispatchModal(true)}
-                disabled={status !== "ready"}
-                variant="contained"
-                sx={{
-                  bgcolor: "#8020E7",
-                  textTransform: "none",
-                  minWidth: "max-content",
-                }}
-              >
-                Send to Dispatch
-              </Button>
-              <Button
-                startIcon={<CheckCircleBroken />}
-                onClick={() => setStatus("acknowledged")}
-                disabled={status !== "sent"}
-                variant="contained"
-                sx={{
-                  bgcolor: "#014DAF",
-                  textTransform: "none",
-                  minWidth: "max-content",
-                }}
-              >
-                Mark as Acknowledged
-              </Button>
-            </div>
-          </div>
-        </div>
-        <SuccessModal
-          open={openDownloadModal}
-          onClose={() => {
-            setStatus("downloaded");
-            setOpenDownloadModal(false);
-          }}
-          description="Production file has been downloaded."
-        />
-        <SuccessModal
-          open={openDispatchModal}
-          onClose={() => {
-            setStatus("sent");
-            setOpenDispatchModal(false);
-          }}
-          description="Card batch successfully sent to dispatch."
-        />
-      </form>
-    </div>
+          <SuccessModal
+            open={openDownloadModal}
+            onClose={() => {
+              setStatus("downloaded");
+              setOpenDownloadModal(false);
+            }}
+            description="Production file has been downloaded."
+          />
+          <SuccessModal
+            open={openDispatchModal}
+            onClose={() => {
+              setStatus("sent");
+              setOpenDispatchModal(false);
+            }}
+            description="Card batch successfully sent to dispatch."
+          />
+        </form>
+      </div>
+    </Suspense>
   );
 };
 
